5fe29bbdbb89facb9a916809dafe9ce883bc8e08
Ankit Kumar Prem <108314952+codexankit@users.noreply.github.com>
2025-09-11 12:44:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+import { extendTheme } from "@chakra-ui/react";
 
+const theme = extendTheme({
+  colors: {
+    scbBlue: {
+      500: "#0473ea", // SCB Blue
+    },
+    scbGreen: {
+      500: "#38d200", // SCB Green
+    },
+  },
+  components: {
+    Button: {
+      defaultProps: {
+        colorScheme: "scbBlue", // all buttons default to SCB Blue
+      },
+    },
+  },
+});
+
+export default theme;

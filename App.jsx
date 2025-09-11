649aa017e2a0cc2b1159a7a1e6c6295e13e85d77
Ankit Kumar Prem <108314952+codexankit@users.noreply.github.com>
2025-09-11 12:47:15 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DocumentUploadPage from "./pages/DocumentUploadPage";
+
+const App = () => {
+  return (
+    <div>
+      <DocumentUploadPage />
+    </div>
+  );
+};
+
+export default App;
